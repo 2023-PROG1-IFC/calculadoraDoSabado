a2caa824337ff91cade0f324b633f7c5eb09d264
ldmfabio <ldmfabio@gmail.com>
2023-11-16 11:16:22 -0300

    1INFO2 - 16/11/2023

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-function clq1() {
-    alert("Você clicou no Botão 1");
-}
-
-let bt2 = document.getElementById("bt2");
-bt2.addEventListener("click", clq2);
-
-function clq2() {
-    alert("Você clicou no Botão 2");
-}
-
 let valor1 = document.querySelector("#valor1");
 let valor2 = document.querySelector("#valor2");
 let resultado = document.querySelector("#resultado");
 
-valor1.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
+let btSoma = document.getElementById("btSomar");
+let btSubtrair = document.getElementById("btSubtrair");
+let btMultiplicar = document.getElementById("btMultiplicar");
+let btDividir = document.getElementById("btDividir");
+
+btSoma.addEventListener("click", function () {
+  soma(Number(valor1.value), Number(valor2.value));
 });
 
-valor2.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
-});
-
-function somar(a, b) {
-    let soma = a + b;
-    resultado.innerHTML = soma;
+function soma(a, b) {
+  resultado.textContent = a + b;
 }
+
+let idioma = document.getElementById("idioma");
+idioma.addEventListener("change", function () {
+  if (this.value == "br") {
+    btSoma.value = "Somar";
+    btSubtrair.value = "Subtrair";
+    btMultiplicar.value = "Multiplicar";
+    btDividir.value = "Dividir";
+  }
+  else if (this.value == "es") {
+    btSoma.value = "Sumar";
+    btSubtrair.value = "Restar";
+    btMultiplicar.value = "Multiplicar";
+    btDividir.value = "Dividir";
+  }
+  else if (this.value == "en") {
+    btSoma.value = "Add";
+    btSubtrair.value = "Subtract";
+    btMultiplicar.value = "Multiply";
+    btDividir.value = "Divide";
+  }
+  else if (this.value == "fr") {
+    btSoma.value = "Ajouter";
+    btSubtrair.value = "Soustraire";
+    btMultiplicar.value = "Multiplier";
+    btDividir.value = "Diviser";
+  }
+});
